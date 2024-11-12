dd3183002316c69d0ae496f6bbd0fd0dd6f8a59f
Adit122022 <adity202212@gmail.com>
2024-11-12 18:45:01 +0530

    Making the counter changing the code of html, css and javascript using chatgpt

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert("do u guys understand git and github")
+let count = 0;
+
+function updateCounter() {
+    document.getElementById("counterValue").innerText = count;
+}
+
+function increase() {
+    count++;
+    updateCounter();
+}
+
+function decrease() {
+    count--;
+    updateCounter();
+}
+
+function reset() {
+    count = 0;
+    updateCounter();
+}
